e11d1d63839d85c0c00eed83596501a11e72910a
denganjun1234@qq.com <denganjun1234@qq.com>
2016-07-12 00:14:26 +0800

    it's work now

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 var fsp = require('fs-promise')
 var path = require('path')
 var jschardet = require('jschardet')
-var inputDir = '../input/'
-var outputDir = '../output'
+var iconv = require('iconv-lite');
+
+// 设置最终输出的文件的编码
 var encodingForResult = 'utf8'
 
-var mod = {
+// 设置要处理的文件夹的路径
+var inputDir = '../input/'
+
+// 设置输出文件夹的路径
+var outputDir = '../output'
+
+
+var txtBeautifier = {
     init() {
         this.handle()
     },
@@ -27,10 +35,13 @@ var mod = {
 
                 fsp.readFile(realPath + file).then(function(data) {
                     var charset = jschardet.detect(data)
+                    charset.encoding = charset.encoding.toLowerCase().replace(/\-/g, '')
                     console.log(charset)
 
-                    mod.writeFile(file, data)
-                        // console.log(data.isEncoding(charset.encoding.toLowerCase().replace(/\-/g, '')))
+                    var result = txtBeautifier.beautify(data, charset.encoding)
+
+                    txtBeautifier.writeFile(file, result)
+
                 }).catch(function(e) {
                     console.log(e)
                 })
@@ -38,6 +49,56 @@ var mod = {
         }).catch(function(e) {
             console.log(e)
         })
+    },
+    beautify(string, encoding) {
+        var result
+
+        string = iconv.decode(string, encoding)
+        result = this.replace(string)
+
+        return result
+    },
+    replace(string) {
+        var result
+            // result = string.replace(/\r\n/g, '')
+        var replaceFn = function(match, offset, string) {
+            var before = string[offset - 1]
+
+            // 前面是“目录” 允许换行
+            if (before === '录') {
+                return '\n'
+            }
+
+            // 前面是“目录” 允许换行
+            if (before === '序') {
+                return '\n'
+            }
+
+            // 后续的10个字符中，出现“第x章”、“第x节”，那么允许换行
+            var after = string.substring(offset + 1, offset + 11)
+            if (after.indexOf('第') > -1 && (after.indexOf('章') > -1 || after.indexOf('节') > -1)) {
+                return '\n'
+            }
+
+            // 前面是句号，允许换行
+            if (before === '。') {
+                return '\n'
+            }
+
+            // 前面是英文 .?! ，允许换行
+            if (before === '.' || before === '?' || before === '!') {
+                return '\n'
+            }
+
+            // 其他情况 换行替换为空
+            return ''
+        }
+
+        result = string.replace(/\r\n/g, replaceFn)
+
+        result = result.replace(/\n/g, replaceFn)
+
+        return result
     },
     writeFile(filename, content) {
         console.log(filename)
@@ -62,4 +123,4 @@ var mod = {
 }
 
 // run 
-mod.init()
+txtBeautifier.init()
